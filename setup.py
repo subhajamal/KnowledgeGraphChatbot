4c2a9d4f6022a2c510bafc9fd8f2e5a96f68208a
Subha <subhasumaiya@gmail.com>
2024-08-01 21:55:47 -0400

    Update setup.py with new configuration

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='knowledgegraph_chatbot',
-    version='0.1',
+    version='0.1.0',  # Consider using semantic versioning for clarity
     packages=find_packages(),
     install_requires=[
         'langchain_openai',
@@ -13,7 +13,22 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'knowledgegraph_chatbot=knowledgegraph_chatbot.chatbot:main',
+            'knowledgegraph_chatbot=knowledgegraph_chatbot.chatbot:main',  # Ensure the 'main' function exists
         ],
     },
+    author='Subha', 
+    author_email='subhasumaiya@gmail.com',
+    description='A package for integrating language models with a Neo4j graph database.',
+    long_description=open('README.md').read(),  # Read from README.md for package description
+    long_description_content_type='text/markdown',  # Format of the long description
+    url='https://github.com/subhajamal/KnowledgeGraphChatbot',  
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',  
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.12', 
+    ],
+    python_requires='>=3.7',  
 )
