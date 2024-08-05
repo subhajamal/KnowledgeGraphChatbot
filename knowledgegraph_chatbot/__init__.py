bdc12c7e1a955b5e71bfbcca42783fc07f15c9af
Subha <60422482+subhajamal@users.noreply.github.com>
2024-08-04 20:47:29 -0400

    Update __init__.py

--- knowledgegraph_chatbot/__init__.py
+++ knowledgegraph_chatbot/__init__.py
@@ -1,3 +1,7 @@
 __version__ = '0.1'
-from knowledgegraph_chatbot import query_neo4j, query_openai_model, handle_query
+
+from .query_neo4j import query_neo4j
+from .query_openai_model import query_openai_model
+from .handle_query import handle_query
+
 __all__ = ['query_neo4j', 'query_openai_model', 'handle_query']
